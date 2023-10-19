a5834f7f066f913e9ea2b41f1d149f15059392a2
Muziwandile Nkomo <muziwandilenko@gmail.com>
2023-10-19 22:08:44 +0200

    second page

--- src/main/resources/static/js/time.js
+++ src/main/resources/static/js/time.js
@@ -9,34 +9,3 @@ function timer() {
 setInterval(timer, 1000);
 
 //function for button
-
-$(document).ready(function() {
-  $('.rating').each(function() {
-    var rating = $(this).find('input[name="rating"]').val();
-
-    $(this).find('span').each(function() {
-      var dataRating = $(this).data('rating');
-
-      if (dataRating <= rating) {
-        $(this).addClass('checked');
-      }
-    });
-  });
-
-  $('.rating span').click(function() {
-    var rating = $(this).data('rating');
-
-    $(this).siblings('input[name="rating"]').val(rating);
-
-    $(this).siblings('span').removeClass('checked');
-    for (var i = 0; i < rating; i++) {
-      $(this).siblings('span').eq(i).addClass('checked');
-    }
-  });
-
-  $('.rating-clear').click(function() {
-    $(this).parent().find('input[name="rating"]').val(0);
-
-    $(this).parent().find('span').removeClass('checked');
-  });
-});
